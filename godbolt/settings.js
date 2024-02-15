6fe67bcef0af9f6afbbc9babcb401266a3604d92
Eczbek <eczbek.void@gmail.com>
2024-02-15 18:56:14 -0500

    Create godbolt/settings.js

---
+++ godbolt/settings.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Godbolt Settings Preset
+// @description Configures things in godbolt settings for me
+// @version 0.1.0
+
+// @author Eczbek
+// @source https://raw.githubusercontent.com/Eczbek/tampermonkey-scripts/main/godbolt/settings.js
+
+// @match https://*.godbolt.org/*
+// @run-at document-start
+// ==/UserScript==
+
+window.localStorage.setItem('settings', '{"defaultLanguage":"c++","allowStoreCodeDebug":false,"alwaysEnableAllSchemes":true,"autoCloseBrackets":true,"autoCloseQuotes":true,"autoSurround":true,"autoIndent":true,"colouriseAsm":false,"colouriseBrackets":true,"compileOnChange":true,"editorsFLigatures":false,"enableCodeLens":true,"enableCommunityAds":false,"enableCtrlStree":false,"enableSharingPopover":false,"executorCompileOnChange":true,"shakeStatusIconOnWarnings":true,"formatOnCompile":false,"hoverShowAsmDoc":true,"hoverShowSource":true,"indefiniteLineHighlight":false,"keepMultipleTabs":false,"keepSourcesOnLangChange":false,"newEditorLastLang":false,"showMinimap":true,"showQuickSuggestions":false,"useCustomContextMenu":true,"useSpaces":false,"useVim":false,"wordWrap":false,"colourScheme":"gray-shade","theme":"darkplus","defaultFontScale":14,"formatBase":"Google","enableCtrlS":"false","delayAfterChange":750,"tabWidth":4,"editorsFFont":"Consolas, \\"Liberation Mono\\", Courier, monospace"}');
